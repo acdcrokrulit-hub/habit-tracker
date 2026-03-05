bdb9a4bd9e1486ddf64d07ab83855efbf7e17fd7
acdcrokrulit <acdcrokrulit@gmail.com>
2026-03-05 19:13:42 +0500

    Add Android and iOS platform support

---
+++ android/app/src/main/kotlin/com/habittracker/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.habittracker.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/main.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+@main
+class Runner: UIApplication {
+}
